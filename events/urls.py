19d8639619cc5f35673b310e042545c0079c05e4
Nassor JA <rossan1tech@gmail.com>
2022-11-21 09:32:24 +0300

    Implementing a calendar

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # path('', views.home, name="home"),
+    
+    # Path Converters ~ they give route to urls
+    # int: numbers, str: strings, 
+    # paths: whole urls & /, 
+    # slugs: hyphen - and underscores _,
+    # UUID: Universally unique identifier
+    path('<int:year>/<str:month>', views.home, name="home"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 # Create your views here.
-def home(request):
- return render(request, 'home.html', {})
+def home(request, year, month):
+    name = "Doha"
+    
+    # the context dictionary after home.html allows us to pass context from the backend to the frontend
+    
+    # return render(request, 'home.html', {"fname": name})
+    
+    # return render(request, 'home.html', {"fname": name, "year": year, "month": month})
+    
+    # Capitalizes the first letter in the month name/ Matches the name to the default lib name in the url
+    month = month.title()
+    # Convert month from name to calendar
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    
+    # creating a calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+    
+    # get current year
+    now = datetime.now()
+    current_year = now.year
+    
+    # get current time
+    current_time = now.strftime('%I:%M:%S %p')
+    
+    return render(request, 'home.html', {"fname": name, 
+                             "year": year, "month": month, 
+                             "month_number": month_number, "cal": cal, "current_year": current_year, "current_time": current_time})
