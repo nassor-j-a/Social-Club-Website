58e48cd95f515bf2ecfbcf98a6670bf2844e83d5
Nassor JA <rossan1tech@gmail.com>
2022-11-24 23:32:09 +0300

    Modifying the Django Admin area

--- events/admin.py
+++ events/admin.py
@@ -4,6 +4,26 @@ from .models import SocialClubUser
 from .models import Event
 
 # Register your models here.
-admin.site.register(Venue)
+# admin.site.register(Venue)
 admin.site.register(SocialClubUser)
-admin.site.register(Event)
+# admin.site.register(Event)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    # changes the display of items on the main page
+    list_display = ('name', 'address', 'phone')
+    
+    # makes the ordering(of the name field) be in an alpabetically order
+    # ('-name') inverts the ordering(from Z to A)
+    ordering = ('name',)
+    search_fields = ('name', 'address')
+
+# admin.site.register(Venue, VenueAdmin)
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name', 'venue'), 'event_date', 'description', 'manager')
+    list_display = ('name', 'event_date', 'venue')
+    # applying a filter
+    list_filter = ('event_date', 'venue')
+    ordering = ('-event_date',)
--- events/models.py
+++ events/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField('Zip Code', max_length=15)
-    phone = models.CharField('Contact Phone', max_length=25)
-    web = models.URLField('Website Address')
-    email_address = models.EmailField('Email Address')
+    phone = models.CharField('Contact Phone', max_length=25, blank=True)
+    web = models.URLField('Website Address', blank=True)
+    email_address = models.EmailField('Email Address', blank=True)
     
     def __str__(self):
         return self.name
@@ -23,9 +23,12 @@ class SocialClubUser(models.Model):
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=255)
     event_date = models.DateTimeField('Event Date')
-    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, blank=True, null=True)
     # venue = models.CharField(max_length=120)
-    manager = models.CharField(max_length=60)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, blank=True, null=True)
+    
+    # on_delete=models.SET_NULL -> IF A MANAGER GETS DELETED THE MANAGER FIELD IN EVENTS GETS SET TO NULL
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    
     # if no description then it can be left blank
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(SocialClubUser, blank=True)
